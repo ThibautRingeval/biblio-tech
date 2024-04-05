244cce49e0f4e40a0edc9d9242d58264176e6051
gmeline <152296568+gmeline@users.noreply.github.com>
2024-04-05 11:31:23 +0200

    Update category.component.ts

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -1,18 +1,60 @@
-<div *ngFor="let category of categories">
-    <span>{{ category.name }}</span>
-    <button (click)="startEditCategory(category)">Modifier</button>
-    <button (click)="deleteCategory(category.id)">Supprimer</button>
-  </div>
-  
-  <div *ngIf="isEditing">
-    <form (submit)="updateCategory()">
-      <input type="text" [(ngModel)]="selectedCategory.name">
-      <button type="submit">Enregistrer</button>
-      <button type="button" (click)="cancelEditCategory()">Annuler</button>
-    </form>
-  </div>
-  
-  <form (submit)="createCategory()">
-    <input type="text" [(ngModel)]="newCategory.name" placeholder="Nouvelle Catégorie">
-    <button type="submit">Ajouter</button>
-  </form>
+import { Component, OnInit } from '@angular/core';
+import { Category } from '../models/category.model';
+import { CategoryService } from './category.service';
+
+@Component({
+  selector: 'app-categories',
+  templateUrl: './categories.component.html',
+  styleUrls: ['./categories.component.css']
+})
+export class CategoriesComponent implements OnInit {
+  categories: Category[] = [];
+  newCategory: Category = { id: 0, name: '' };
+  isEditing = false;
+  selectedCategory: Category | null = null;
+
+  constructor(private categoryService: CategoryService) { }
+
+  ngOnInit(): void {
+    this.loadCategories();
+  }
+
+  loadCategories(): void {
+    this.categoryService.getAllCategories().subscribe(categories => {
+      this.categories = categories;
+    });
+  }
+
+  createCategory(): void {
+    this.categoryService.createCategory(this.newCategory).subscribe(() => {
+      this.loadCategories();
+      this.newCategory = { id: 0, name: '' };
+    });
+  }
+
+  startEditCategory(category: Category): void {
+    this.selectedCategory = { ...category };
+    this.isEditing = true;
+  }
+
+  cancelEditCategory(): void {
+    this.selectedCategory = null;
+    this.isEditing = false;
+  }
+
+  updateCategory(): void {
+    if (this.selectedCategory) {
+      this.categoryService.updateCategory(this.selectedCategory).subscribe(() => {
+        this.loadCategories();
+        this.selectedCategory = null;
+        this.isEditing = false;
+      });
+    }
+  }
+
+  deleteCategory(id: number): void {
+    this.categoryService.deleteCategory(id).subscribe(() => {
+      this.loadCategories();
+    });
+  }
+}
